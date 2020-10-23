db6975b287f30d61c09eb6ae2e3e27405155f838
GBondarek <georgettebondarek@gmail.com>
2020-10-22 20:46:43 -0400

    added js

--- script.js
+++ script.js
@@ -39,11 +39,34 @@ var quizQuestions = [
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
 	function showQuestions(questions, quizContainer){
-		// code will go here
-	}
+    var output= [];
+    var answers;
+  }
+  for(var i=0; i<questions.length; i++){
 
+      answers = [];
+
+  for(letter in questions[i].answers){
+
+    answers.click(
+      '<label>'
+      +'<input type="radio" name="question'+i+'" value="'+letter+'">'
+      +letter + ': '
+      +questions[i].answers[letter]
+    + '</label>'
+
+    );
+  }
+  
+  output.push(
+    '<div class="question">' + questions[i].question + '</div>'
+    + '<div class="answers">' + answers.join('') + '</div>'
+  );
+}
+quizContainer.innerHTML = output.join('');
+}
 	function showResults(questions, quizContainer, resultsContainer){
-		// code will go here
+	
 	}
 
 	// show the questions
